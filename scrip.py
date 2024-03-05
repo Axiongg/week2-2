d26b170c3beb81503ce3826a3e840567485a5054
Axiongg <161007911+Axiongg@users.noreply.github.com>
2024-03-05 16:53:03 +0800

    Add files via upload

---
+++ scrip.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 16:37:43 2024
+
+@author: THULIB
+"""
+
+a=str(input("請輸入姓名:"))
+b=int(input("請輸入年齡:"))
+c=float(input("請輸入身高:"))
+d=str(input("請輸入喜愛的顏色:"))
+print(a,",",b,"歲,","身高",c,"米,","喜愛的顏色是",d,".")
