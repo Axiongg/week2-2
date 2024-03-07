1a1af5b175b3c251e3ee9c2ef4673e3108c253e8
Axiongg <161007911+Axiongg@users.noreply.github.com>
2024-03-07 08:33:46 +0800

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a=str(input("請輸入姓名:"))
+b=int(input("請輸入年齡:"))
+c=float(input("請輸入身高(米):"))
+d=str(input("請輸入喜愛的顏色:"))
+print(a,",",b,"歲,","身高",c,"米,","喜愛的顏色是",d,".")
